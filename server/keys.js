81c0fb86a87866aedad25c41d32e7d1f6d9c49a6
parag98 <parag.poddar.77@gmail.com>
2023-02-20 22:11:05 +0530

    all

---
+++ server/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:"mongodb+srv://parag123:Parag123@cluster0.ihvk8.mongodb.net/trans_system?retryWrites=true&w=majority",
+    JWT_SECRET:"transportationproject",
+   
+    
+}
